f1d915b6c5f78555121a5cefb1c17724ee357150
KXJiao <kxjiao@gmail.com>
2018-07-07 16:33:36 -0400

    import

--- researchtime/summary.py
+++ researchtime/summary.py
@@ -3,6 +3,7 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 from werkzeug.utils import secure_filename
+import os
 import textract
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'doc', 'docx', 'html', 'htm', 'epub'])
