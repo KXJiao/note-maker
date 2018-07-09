66bd94d234462c2fa077fa9ef18b6745c00f8f88
KXJiao <kxjiao@gmail.com>
2018-07-09 10:52:11 -0400

    f

--- researchtime/summary.py
+++ researchtime/summary.py
@@ -19,39 +19,43 @@ def allowed_file(filename):
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def summarize(text):
-    parser = PlaintextParser.from_string(text, Tokenizer("english"))
+    parser = PlaintextParser.from_string(text, Tokenizer('english'))
     summarizer = LexRankSummarizer()
     summary = summarizer(parser.document, 5)
-    summarized = ""
+    summarized = ''
     for sentence in summary:
-        summarized = summarized + str(sentence) + '\n'
+        summarized = summarized + str(sentence) + '\\n'
     return summarized
 
 bp = Blueprint('summary', __name__)
 @bp.route('/', methods=['GET', 'POST'])
 def index():
-    # if request.method == 'POST':
-    #     if 'file' not in request.files:
-    #         flash('No file part')
-    #         return redirect(request.url)
-    #     file = request.files['file']
-    #     if file.filename == '':
-    #         flash('No selected file')
-    #         return redirect(request.url)
-    #     if file and allowed_file(file.filename):
-    #         filename = secure_filename(file.filename)
-    #         print(file.filename)
-    #         print(filename)
-    #         print(app.config['UPLOAD_FOLDER'])
-    #         print(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    #         basedir = os.path.abspath(os.path.dirname(__file__))
-    #         file.save(os.path.join(basedir, app.config['UPLOAD_FOLDER'], filename))
-    #         print(url_for('uploaded_file', filename=filename))
-    #         return redirect(url_for('uploaded_file',
-    #                                 filename=filename))
-    processed = ""
+    processed = ''
     if request.method == 'POST':
         raw_text = request.form['text']
-        processed = summarize(raw_text)
+        if raw_text != '':
+            processed = summarize(raw_text)
+
+            
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            print(file.filename)
+            print(filename)
+            print(app.config['UPLOAD_FOLDER'])
+            print(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            basedir = os.path.abspath(os.path.dirname(__file__))
+            file.save(os.path.join(basedir, app.config['UPLOAD_FOLDER'], filename))
+            print(url_for('uploaded_file', filename=filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+
+        
     return render_template('summary/index.html', processed = processed)
 
