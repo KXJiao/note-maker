6d43c16d7c15d470175c1e9337ab3858a1f4e556
KXJiao <kxjiao@gmail.com>
2018-07-10 10:40:24 -0400

    a

--- researchtime/summary.py
+++ researchtime/summary.py
@@ -6,7 +6,7 @@ from werkzeug.exceptions import abort
 from werkzeug.utils import secure_filename
 import os
 from os.path import abspath
-import LexRank
+from . import LexRank
 import uuid
 import textract
 
